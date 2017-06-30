1f6b5858aa243f3b52cd4d746e944fcc57e0cb06
Denis Tchaikovsky <dionis.tchaikovsky@gmail.com>
2017-06-30 13:00:44 +0300

    remove credentials + complete response-booking_links dependency logic

--- GetGoing/app/models/response.rb
+++ GetGoing/app/models/response.rb
@@ -3,13 +3,15 @@ class Response < ApplicationRecord
   belongs_to :user
 
   has_many :top_responses
-  has_many :booking_links
+  has_many :booking_links, dependent: :destroy
 
   after_save :extract_booking_links
 
   def extract_booking_links
     links = body.scan(/([a-zA-Z]+)#((?:https?):\/\/[\w\d\/.-\?\&\%\-\#]+)/)
-    old_booking_links = booking_links
+    if booking_links.present?
+      booking_links.destroy_all
+    end
     if links.present?
       links.each do |link_pair|
         url_type = link_pair[0].downcase.singularize
--- GetGoing/config/initializers/setup_mail.rb
+++ GetGoing/config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "mail.google.com",
-    :user_name => "kennykandola89@gmail.com",
-    :password => "Steelers32!",
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
     :authentication => :login,
     :enable_starttls_auto => true
 }
