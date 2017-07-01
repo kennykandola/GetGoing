ca6dc6a69fa7a32be7445d13a8754be470c5ec5e
Denis Tchaikovsky <dionis.tchaikovsky@gmail.com>
2017-07-01 10:53:15 +0300

    initial notifications

---
+++ GetGoing/app/models/notification.rb
@@ -0,0 +1,10 @@
+class Notification < ApplicationRecord
+  belongs_to :recipient, class_name: 'User'
+  belongs_to :actor, class_name: 'User'
+  belongs_to :notifiable, polymorphic: true
+
+  scope :unread, -> { where(read_at: nil) }
+  enum action: [:new_response, :recommended_link_upvoted, :new_post_with_matching_place]
+
+  after_commit -> { NotificationRelayJob.perform_later(self) }
+end
--- GetGoing/app/models/user.rb
+++ GetGoing/app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :claims, through: :posts
   has_many :identities, dependent: :destroy
   has_many :votes
+  has_many :notifications, foreign_key: :recipient_id
 
   enum role: [:simple_user, :moderator, :admin]
 
@@ -79,5 +80,7 @@ class User < ApplicationRecord
     end
   end
 
-
+  def unread_notifications
+    Notification.where(recipient: self).unread
+  end
 end
