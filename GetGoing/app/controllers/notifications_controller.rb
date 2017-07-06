3895431018b043b91c8d5ed1d95b69a94af7be15
Denis Tchaikovsky <dionis.tchaikovsky@gmail.com>
2017-07-06 10:12:39 +0300

    order newest notifications on top

--- GetGoing/app/controllers/notifications_controller.rb
+++ GetGoing/app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.order(created_at: :desc)
   end
 
   def mark_as_read
