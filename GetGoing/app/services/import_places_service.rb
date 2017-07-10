97409f2b8577352321dbb9c32f4120d4be6d9a93
Denis Tchaikovsky <dionis.tchaikovsky@gmail.com>
2017-07-10 21:14:41 +0300

    add selection from multiple places using coordinates

--- GetGoing/app/services/import_places_service.rb
+++ GetGoing/app/services/import_places_service.rb
@@ -4,12 +4,16 @@ class ImportPlacesService
   def initialize(params)
     @user = params[:user]
     @graph_api = @user.facebook_graph(@user.facebook.accesstoken)
+    @google_place_api = GooglePlaces::Client.new(ENV['GOOGLE_MAPS_API'])
   end
 
   def add_tagged_places
     tagged_places = @graph_api.get_object('me/tagged_places')
     tagged_places.each do |data|
-      new_potential_place = get_potential_place(data['place']['name'])
+      name = data['place']['name']
+      coordinates = data['place']['location']
+      new_potential_place = get_potential_place(name, coordinates)
+      return nil if new_potential_place.blank?
       existing_place = Place.where(google_place_id: new_potential_place.google_place_id)
       if @user.places.where(google_place_id: new_potential_place.google_place_id).blank? && existing_place.blank?
         new_potential_place.save!
@@ -20,14 +24,30 @@ class ImportPlacesService
     end
   end
 
-  def get_potential_place(name)
+  def get_potential_place(name, coordinates=nil)
     name = normalize(name)
-    google_place_api = GooglePlaces::Client.new(ENV['GOOGLE_MAPS_API'])
-    found_places = google_place_api.spots_by_query(name)
-    google_place_id = found_places.first.place_id
-    address = found_places.first.formatted_address
-    name = found_places.first.name
+    found_places = @google_place_api.spots_by_query(name)
+    return nil if found_places.count == 0
+    if found_places.count == 1 ||
+       (found_places.count > 1 && found_places.first.types == ["locality", "political"]) || # if first place is city
+       (found_places.count > 1 && coordinates.blank?) # facebook don't provide coordinates for hometown and current location
+      new_place = found_places.first
+    elsif found_places.count > 1
+      new_place = nearest_place(name, coordinates)
+    end
+    google_place_id = new_place.place_id
+    address = new_place.formatted_address
+    name = new_place.name
     Place.new(google_place_id: google_place_id, address: address, name: name)
+  end
+
+  def nearest_place(name, coordinates)
+    nearest_places = @google_place_api.predictions_by_input(name,
+                                           lat: coordinates['latitude'],
+                                           lng: coordinates['longitude'],
+                                           radius: 5000,
+                                           types: 'geocode')
+    @google_place_api.spot(nearest_places.first.place_id)
   end
 
   def normalize(name)
@@ -38,7 +58,8 @@ class ImportPlacesService
   def add_hometown
     hometown_data = @graph_api.get_object('me?fields=hometown')
     if hometown_data['hometown'].present?
-      new_potential_place = get_potential_place(hometown_data['hometown']['name'])
+      name = hometown_data['hometown']['name']
+      new_potential_place = get_potential_place(name)
       existing_place = Place.where(google_place_id: new_potential_place.google_place_id)
       if existing_place.present?
         PlaceUserRelation.create(user: @user, place: existing_place.first, relation: 'hometown')
@@ -52,7 +73,8 @@ class ImportPlacesService
   def add_location
     location_data = @graph_api.get_object('me?fields=location')
     if location_data['location'].present?
-      new_potential_place = get_potential_place(location_data['location']['name'])
+      name = location_data['location']['name']
+      new_potential_place = get_potential_place(name)
       existing_place = Place.where(google_place_id: new_potential_place.google_place_id)
       if existing_place.present?
         PlaceUserRelation.create(user: @user, place: existing_place.first, relation: 'location')
